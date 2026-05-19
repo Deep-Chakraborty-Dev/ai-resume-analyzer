ec06338fef589fdd65ceee251e33b8d6e34b7ab7
Deep Chakraborty <iamdeepchakraborty19112000@gmail.com>
2026-05-19 18:46:16 +0530

    cloud-PDFtoIMG-ATS

---
+++ app/routes/upload.tsx
@@ -0,0 +1,238 @@
+// Upload.tsx
+
+import { type FormEvent, useState } from 'react';
+import Navbar from '~/components/Navbar';
+import FileUploader from '~/components/FileUploader';
+import { usePuterStore } from '~/lib/puter';
+import { useNavigate } from 'react-router';
+import { convertPdfToImage } from '~/lib/pdf2img';
+import { generateUUID } from '~/lib/utils';
+import { prepareInstructions } from '../../constants';
+
+const Upload = () => {
+  const { fs, ai, kv } = usePuterStore();
+
+  const navigate = useNavigate();
+
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [statusText, setStatusText] = useState('');
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileSelect = (file: File | null) => {
+    console.log('SELECTED FILE:', file);
+    setFile(file);
+  };
+
+  const handleAnalyze = async ({
+    companyName,
+    jobTitle,
+    jobDescription,
+    file,
+  }: {
+    companyName: string;
+    jobTitle: string;
+    jobDescription: string;
+    file: File;
+  }) => {
+    try {
+      setIsProcessing(true);
+
+      console.log('START ANALYZE');
+
+      setStatusText('Uploading PDF...');
+
+      const uploadedFile = await fs.upload([file]);
+
+      console.log('UPLOADED PDF:', uploadedFile);
+
+      if (!uploadedFile) {
+        setStatusText('Failed to upload PDF');
+        return;
+      }
+
+      setStatusText('Converting PDF to image...');
+
+      console.log('START PDF CONVERSION');
+
+      const imageFile = await convertPdfToImage(file);
+
+      console.log('PDF CONVERSION RESULT:', imageFile);
+
+      if (!imageFile.file) {
+        console.error(imageFile.error);
+
+        setStatusText(imageFile.error || 'Failed to convert PDF');
+
+        return;
+      }
+
+      setStatusText('Uploading image...');
+
+      const uploadedImage = await fs.upload([imageFile.file]);
+
+      console.log('UPLOADED IMAGE:', uploadedImage);
+
+      if (!uploadedImage) {
+        setStatusText('Failed to upload image');
+        return;
+      }
+
+      setStatusText('Preparing data...');
+
+      const uuid = generateUUID();
+
+      const data = {
+        id: uuid,
+        resumePath: uploadedFile.path,
+        imagePath: uploadedImage.path,
+        companyName,
+        jobTitle,
+        jobDescription,
+        feedback: '',
+      };
+
+      await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+      console.log('DATA SAVED');
+
+      setStatusText('Analyzing resume...');
+
+      const feedback = await ai.feedback(
+        uploadedFile.path,
+        prepareInstructions({
+          jobTitle,
+          jobDescription,
+        }),
+      );
+
+      console.log('AI FEEDBACK:', feedback);
+
+      if (!feedback) {
+        setStatusText('Failed to analyze resume');
+        return;
+      }
+
+      const feedbackText =
+        typeof feedback.message.content === 'string'
+          ? feedback.message.content
+          : feedback.message.content[0].text;
+
+      data.feedback = JSON.parse(feedbackText);
+
+      await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+      console.log('FINAL DATA:', data);
+
+      setStatusText('Analysis complete');
+
+      navigate(`/resume/${uuid}`);
+    } catch (error) {
+      console.error('UPLOAD PAGE ERROR:', error);
+
+      setStatusText(`Unexpected error: ${error}`);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget.closest('form');
+
+    if (!form) return;
+
+    const formData = new FormData(form);
+
+    const companyName = formData.get('company-name') as string;
+
+    const jobTitle = formData.get('job-title') as string;
+
+    const jobDescription = formData.get('job-description') as string;
+
+    if (!file) {
+      alert('Please upload a PDF');
+      return;
+    }
+
+    handleAnalyze({
+      companyName,
+      jobTitle,
+      jobDescription,
+      file,
+    });
+  };
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Smart feedback for your dream job</h1>
+
+          {isProcessing ? (
+            <>
+              <h2>{statusText}</h2>
+
+              <img src="/images/resume-scan.gif" className="w-full" />
+            </>
+          ) : (
+            <h2>Drop your resume for an ATS score</h2>
+          )}
+
+          {!isProcessing && (
+            <form
+              id="upload-form"
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-4 mt-8"
+            >
+              <div className="form-div">
+                <label htmlFor="company-name">Company Name</label>
+
+                <input
+                  type="text"
+                  name="company-name"
+                  placeholder="Company Name"
+                  id="company-name"
+                />
+              </div>
+
+              <div className="form-div">
+                <label htmlFor="job-title">Job Title</label>
+
+                <input
+                  type="text"
+                  name="job-title"
+                  placeholder="Job Title"
+                  id="job-title"
+                />
+              </div>
+
+              <div className="form-div">
+                <label htmlFor="job-description">Job Description</label>
+
+                <textarea
+                  rows={5}
+                  name="job-description"
+                  placeholder="Job Description"
+                  id="job-description"
+                />
+              </div>
+
+              <div className="form-div">
+                <label htmlFor="uploader">Upload Resume</label>
+
+                <FileUploader onFileSelect={handleFileSelect} />
+              </div>
+
+              <button className="primary-button" type="submit">
+                Analyze Resume
+              </button>
+            </form>
+          )}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Upload;
